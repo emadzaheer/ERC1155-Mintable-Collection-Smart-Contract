bb80f1c5ef87f6eaee1cc76be300419f1026304a
emadzaheer <87974481+emadzaheer@users.noreply.github.com>
2022-12-09 15:27:18 +0500

    added comments for clarity

--- ERC1155-NFTcollection.sol
+++ ERC1155-NFTcollection.sol
@@ -9,16 +9,18 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
 contract MTKFinale is ERC1155, Ownable, Pausable, ERC1155Supply, PaymentSplitter {
-    uint256 public publicPrice = 0.007 ether;
-    uint256 public maxSupply = 3000;
-    uint256 allowListMintPrice = 0.003 ether; 
+    uint256 public publicPrice = 0.007 ether;           
+    uint256 public maxSupply = 3000;                        //max supply per token
+    uint256 allowListMintPrice = 0.003 ether;               // discounted price for allow list addresses.
     bool public PublicMintOpen = false;
     bool public AllowListMintOpen = false;
-    mapping(address=> bool) public m_allowList;
+    mapping(address=> bool) public m_allowList;     
     mapping(address => mapping(uint256 => uint256)) public m_maxTokenTypePerWallet; 
 
+    //@param _payees is an array which takes input in the form: ["address1", "address2"]
+    //@param _shares is an array. input form: [uint, uint] ///out of 100% 
     constructor(
-        address[] memory _payees,
+        address[] memory _payees,                                         
         uint256[] memory _shares
     )
         ERC1155("ipfs://Qmaa6TuP2s9pSKczHF4rwWhTKUdygrrDs8RmYYqCjP3Hye/")
@@ -46,7 +48,8 @@ contract MTKFinale is ERC1155, Ownable, Pausable, ERC1155Supply, PaymentSplitter
         return string(abi.encodePacked(super.uri(_id), Strings.toString(_id), ".json" ));
     }
 
-
+    //@param: id: the unique id for each token.
+    //@param: amount: the num of copies for token with (id). 
     function mint_public(uint256 id, uint256 amount) public payable {
         //removed onlyOwner bec we want public to mint    
         require(PublicMintOpen, "Public minting has not been opened yet");
@@ -72,11 +75,11 @@ contract MTKFinale is ERC1155, Ownable, Pausable, ERC1155Supply, PaymentSplitter
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 
-
-    function withdraw(address _addr) external payable onlyOwner{          //owner's way to extract money from the contract balance.
-        uint256 balance = address(this).balance;
-        payable(_addr).transfer(balance);
-    }
+    //not needed if payment splitter is used/
+    // function withdraw(address _addr) external payable onlyOwner{          //owner's way to extract money from the contract balance.
+    //     uint256 balance = address(this).balance;
+    //     payable(_addr).transfer(balance);
+    // }
 
 
     function allowListMint(uint256 id, uint256 amount ) public payable {
@@ -90,15 +93,15 @@ contract MTKFinale is ERC1155, Ownable, Pausable, ERC1155Supply, PaymentSplitter
         _mint(msg.sender, id, amount, "");
     } 
 
-    //create mapping to store the allow addrsses
-    // 
+    //2 bool params are used to allow (allow list minting) and (public minting) 
     function PermitForAllowLists( bool approvalBoolPublic, bool approvalBoolAllowList ) external onlyOwner {    //owner allows whether public or alllow minting should be open
         PublicMintOpen = approvalBoolPublic;                 //owner will set 
         AllowListMintOpen = approvalBoolAllowList;
     }
 
+    //addresses should be passsed as: ["0x8s2b27t73", "0x6gftsiasdgiyde"]
     function storeAllowList(address[] calldata allowListAddresses) external onlyOwner {
-        //addresses should be passsed as: ["0x8s2b27t73", "0x6gftsiasdgiyde"]
+        
         for (uint256 i = 0 ; i < allowListAddresses.length ; i++ ){
             m_allowList[ allowListAddresses[i] ] = true;
         }
